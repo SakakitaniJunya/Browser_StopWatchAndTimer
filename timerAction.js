66211c5fe3cc1e1d64a99ca8ae3c890b71c6e8cb
Sakaki <sakajun0208@gmail.com>
2023-05-01 16:54:43 +0900

    timer develop

--- timerAction.js
+++ timerAction.js
@@ -21,51 +21,44 @@ function decrementSeconds() {
 }
 
 function updateTimerDisplay() {
-  // 経過時間を、時、分、秒に分類
-  const minutes = Math.floor((timerSeconds % 3600000) / 60000);
-  const seconds = Math.floor(((timerSeconds % 3600000) % 60000) / 1000);
-
-  // 表示用の時間に成形
-  const formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
-    .toString()
-    .padStart(2, "0")}`;
-
+  const formattedTime = formatTime(timerSeconds);
   document.getElementById("stopWatch-time").textContent = formattedTime;
-
-  document.title = `${formattedTime}`;
+  document.title = formattedTime;
 }
 
-// Startボタンを押した時の処理
 function startTimer() {
   if (intervalID) {
     clearInterval(intervalID);
   }
+
   document.getElementById("input-minutes").disabled = true;
   document.getElementById("input-minutes").hidden = true;
-  if (inputMinutes == 0) {
+
+  if (inputMinutes === 0) {
     inputMinutes = parseInt(document.getElementById("input-minutes").value, 10);
     timerSeconds = inputMinutes * 60;
   }
 
   updateTitle();
   document.getElementById("stopWatch-time").style.height = "auto";
-  intervalID = setInterval(function () {
-    updateTimerDisplay();
+
+  intervalID = setInterval(() => {
     decrementSeconds();
+    updateTimerDisplay();
   }, 1000);
 }
 
-// Stopボタンを推したときの処理
 function stopTimer() {
   clearInterval(intervalID);
 }
 
-// Resetボタンを推したときの処理
 function resetTimer() {
   clearInterval(intervalID);
   document.getElementById("input-minutes").disabled = false;
   document.getElementById("input-minutes").hidden = false;
   document.getElementById("input-minutes").value = "";
+  document.getElementById("stopWatch-time").style.height = 0;
+
   timerSeconds = 0;
   updateTitle();
 }
