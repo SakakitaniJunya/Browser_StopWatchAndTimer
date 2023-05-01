cfceda7890d1d4339a9753aeaa52ce254c125b11
Sakaki <sakajun0208@gmail.com>
2023-05-01 11:29:43 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let startTime;
+let elapsedTime = 0;
+let timeInterval;
+
+const timeDisplay = document.getElementById("time");
+
+function updateTimeDisplay() {
+  // 経過時間を、時、分、秒に分類
+  const hours = Math.floor(elapsedTime / 3600000);
+  const minutes = Math.floor((elapsedTime % 3600000) / 60000);
+  const seconds = Math.floor(((elapsedTime % 3600000) % 60000) / 1000);
+
+  // 表示用の時間に成形
+  const formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+  timeDisplay.textContent = formattedTime;
+  document.title = `ストップウォッチ - ${formattedTime}`;
+}
+
+// 1. ストップウォッチ開始
+function StopWatchStart() {
+  // 開始時間を記録
+  startTime = Date.now() - elapsedTime;
+
+  // タイマーをセット
+  timerinterval = setInterval(function () {
+    elapsedTime = Date.now() - startTime;
+    updateTimeDisplay();
+  }, 1000);
+}
+
+// 2. ストップウォッチ終了
+function StopWatchStop() {
+  clearInterval(timerInterval);
+}
+
+// 3. ストップウォッチリセット
+function StopWatchReset() {
+  elapsedTime = 0;
+  updateTimeDisplay();
+}
+
+// 4. HTMLのstart、stop、resetボタンにそれぞれイベントリスナーを追加
+document.getElementById("start").addEventListener("click", StopWatchStart);
+document.getElementById("stop").addEventListener("click", StopWatchStop);
+document.getElementById("reset").addEventListener("click", StopWatchReset);
